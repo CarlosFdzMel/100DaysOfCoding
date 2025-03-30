a6342c1442ab27e92a55ce3da79af3ea5b3a8c70
Carlitos Fernández <carlos.fernandez.melgarejo09@gmail.com>
2025-03-29 19:47:39 -0600

    Data Type in Python

---
+++ Excercises/datatype.py
@@ -0,0 +1,11 @@
+#Diferences between the data types
+
+#str() cadena de caracteres 
+#bool() opcion binaria True/False
+#int() numero entero
+#float() numero con decimal
+
+name= input("Enter your name ")
+length = len(name)
+letters= str(length)
+print("Number of letters in your name: " + letters)
