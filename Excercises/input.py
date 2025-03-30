d8c3f5c3307030f8a51200fa4e2d24c8ff83606b
Carlitos Fernández <carlos.fernandez.melgarejo09@gmail.com>
2025-03-29 19:49:38 -0600

    Use of input function in Python

---
+++ Excercises/input.py
@@ -0,0 +1,13 @@
+# Use of input() function
+#input("What is your name?")
+#print("Hello " + input("What is your name?") + "!")
+
+#Use of variables
+name = input("What is your name? ")
+print("Hello " + name)
+print (len(name)) #len() returns the length of what is inside ()
+
+username= input("What is your name? ")
+length = len(username)
+print(length)
+
