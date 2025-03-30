fab201661f2b1a3019d0e7bb8190b15d8154c64a
Carlitos Fernández <carlos.fernandez.melgarejo09@gmail.com>
2025-03-29 19:50:25 -0600

    Use of "for" loop

---
+++ Excercises/loops.py
@@ -0,0 +1,17 @@
+# Encuentra el puntaje más alto usando un ciclo
+
+scores = [180, 124, 165, 173, 189, 169, 146]
+
+max_score = 0
+for score in scores: 
+    if score > max_score:
+        max_score = score
+print(max_score)
+
+
+# Valor de Gauss
+total = 0
+for number in range(1, 101):
+    total += number
+print(total)
+    
