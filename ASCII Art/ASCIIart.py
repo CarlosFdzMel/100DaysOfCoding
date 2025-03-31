2d18597a41bcad9adad627800ee2a0f988f6e98e
Carlitos Fernández <carlos.fernandez.melgarejo09@gmail.com>
2025-03-31 13:54:28 -0600

    ASCII Art
    This code storage the ASCII Art I used for projects. It starts at daily project 8.

---
+++ ASCII Art/ASCIIart.py
@@ -0,0 +1,13 @@
+# All the ASCII art i used for the 100 days of programming bootcamp
+
+caesar_cypher_logo = (''' 
+  ad8888888888ba
+ dP'         `"8b,
+ 8  ,aaa,       "Y888a     ,aaaa,     ,aaa,  ,aa,
+ 8  8' `8           "88baadP""""YbaaadP"""YbdP""Yb
+ 8  8   8              """        """      ""    8b
+ 8  8, ,8         ,aaaaaaaaaaaaaaaaaaaaaaaaddddd88P
+ 8  `"""'       ,d8""
+ Yb,         ,ad8"   
+  "Y8888888888P"
+''')
